aaaaedf329850ba3a22ddf628728646d9da53511
idohatam <idohatam@gmail.com>
2023-01-09 12:39:00 -0800

    Added the input interface needed for the app

--- Allele_dynamics/app.R
+++ Allele_dynamics/app.R
@@ -2,9 +2,9 @@
 library("shiny")
 library("dplyr")
 library("tidyr")
-library("DT")
 library("ggplot2")
 library("plotly")
+library("shinycssloaders")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -15,11 +15,32 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+          
+          selectInput("sc", "How do the alleles interact?",
+                      c("The new allele is recessive", "The new allele is dominanat",
+                        "The alleles are codominant", "Overdominance of the hetrozygot", 
+                        "Underdominance of the heterozygot")),  
+          sliderInput("bins",
+                        "Proportion of new allele:",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 30),
+            uiOutput("Old"),
+            sliderInput("psize", "Population size:",
+                        min =  1000,
+                        max = 100000,
+                        step = 100,
+                        value = 10000),
+            sliderInput("s", "How goos is the new allele?:",
+                        min =  -1,
+                        max = 1,
+                        step = 0.001,
+                        value = 0.001),
+            sliderInput("t", "How sucessful is the heterozygot?:",
+                        min =  -1,
+                        max = 1,
+                        step = 0.001,
+                        value = 0.001)
         ),
 
         # Show a plot of the generated distribution
@@ -31,6 +52,11 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  output$Old <- renderUI({
+    sliderInput("Old", "Proportions of old allele", min = 0,
+                max = 1,
+                value = (1-(input$bins/100)), step = 0.01)
+  })
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
